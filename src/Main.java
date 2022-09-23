fd37860a6f4113322c7d8cf717aa9e06a611bd53
hjf <marinaff-xiii@mail.ru>
2022-09-23 16:04:53 +0500

    ДЗ строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        fullName = fullName.toUpperCase();
+        System.out.println(fullName);
+        fullName = "Иванов Семён Семёнович";
+        if (fullName.contains("ё")) {
+            System.out.println("Буква ё присутствует");
+            fullName = fullName.replace("ё", "е");
+                System.out.println(fullName);
+            }
+        }
     }
-}
